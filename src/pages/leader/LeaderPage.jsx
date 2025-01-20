665a81bd40fd80d200d85635ac659573b463ae5d
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-01-21 00:14:16 +0900

    feat: 리더 페이지

--- src/pages/leader/LeaderPage.jsx
+++ src/pages/leader/LeaderPage.jsx
@@ -1,25 +1,63 @@
+import styled from "styled-components";
 import defaultImg from "../../assets/icons/LeaderWOW.svg";
 import LeaderCard from "../../components/leader/LeaderCard";
 
+const LeaderContainer = styled.div`
+  background: black;
+  padding: 5rem 12.5rem 10rem 12.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 4rem;
+  margin-bottom: 8.13rem;
+`;
+
+const SectionTitle = styled.div`
+  color: #90e6c9;
+  text-align: center;
+  font-family: "Pretendard Variable";
+  font-size: 2.5rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 170%; /* 4.25rem */
+  letter-spacing: -0.025rem;
+`;
+
+const SectionDetail = styled.div`
+  color: #818989;
+  text-align: center;
+  font-family: "Pretendard Variable";
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 170%; /* 2.125rem */
+  letter-spacing: -0.0125rem;
+`;
+
 const LeaderPage = () => {
+  <LeaderCard
+    img={defaultImg}
+    role="회장"
+    name="이름"
+    nickname="별명"
+    major="컴퓨터공학과"
+  />;
+
   return (
-    <div
-      style={{
-        background: "black",
-        marginTop: "5rem",
-        display: "flex",
-        gap: "1rem",
-        justifyContent: "center",
-      }}
-    >
-      <LeaderCard
-        img={defaultImg}
-        role="회장"
-        name="박도연"
-        nickname="도도"
-        major="홍익대학교 컴퓨터공학과"
-      />
-    </div>
+    <LeaderContainer>
+      <Section>
+        <SectionTitle>운영진</SectionTitle>
+        <SectionDetail>UMC를 이끌어갈 운영진을 소개합니다</SectionDetail>
+      </Section>
+    </LeaderContainer>
   );
 };
 
