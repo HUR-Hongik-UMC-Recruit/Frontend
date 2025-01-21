b04666f85bf276b845d354a85ece68ba56d8f261
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-01-22 02:14:05 +0900

    feat: 라우팅 적용 및 /main 홈페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import "./App.css";
 import GlobalStyle from "./GlobalStyles";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
-import HomePage from "./pages/home/HomePage";
-import RecruitmentPage from "./pages/recruitment/RecruitmentPage";
-import ApplicationPage from "./pages/application/ApplicationPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import AdminJoinPage from "./pages/admin/AdminJoinPage";
-import AdminLoginPage from "./pages/admin/AdminLoginPage";
-
-import { EmailProvider } from "./contexts/EmailContext";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routing from "./pages/Routing";
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/recruitment" element={<RecruitmentPage />} />
-        <Route
-          path="/apply"
-          element={
-            <EmailProvider>
-              <ApplicationPage />
-            </EmailProvider>
-          }
-        />
-        <Route path="/join" element={<AdminJoinPage />} />
-        <Route path="/login" element={<AdminLoginPage />} />
-        {/* <Route path="/admin" element={} /> */}
-      </Routes>
-      <Footer />
+      <Routing />
     </Router>
   );
 }
---
+++ src/pages/Routing.jsx
@@ -0,0 +1,51 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Header from "../Header";
+import Footer from "../Footer";
+import HomePage from "./home/HomePage";
+import NotFoundPage from "./NotFoundPage";
+import RecruitmentPage from "./recruitment/RecruitmentPage";
+import { EmailProvider } from "../contexts/EmailContext";
+import ApplicationPage from "./application/ApplicationPage";
+import AdminJoinPage from "./admin/AdminJoinPage";
+import AdminLoginPage from "./admin/AdminLoginPage";
+
+function Routing() {
+  // Header와 Footer 표시할 페이지
+  const { pathname } = useLocation();
+  const notHidePages = [
+    "/",
+    "/home",
+    "/main",
+    "/recruitment",
+    "/apply",
+  ].includes(pathname);
+
+  return (
+    <>
+      {notHidePages && <Header />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/main" element={<HomePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+        {/* <Route path="/leader" element={<LeaderPage />} /> */}
+        <Route path="/recruitment" element={<RecruitmentPage />} />
+        <Route
+          path="/apply"
+          element={
+            <EmailProvider>
+              <ApplicationPage />
+            </EmailProvider>
+          }
+        />
+        <Route path="/recruitment" element={<RecruitmentPage />} />
+        <Route path="/join" element={<AdminJoinPage />} />
+        <Route path="/login" element={<AdminLoginPage />} />
+        {/* <Route path="/admin" element={} /> */}
+      </Routes>
+      {notHidePages && <Footer />}
+    </>
+  );
+}
+
+export default Routing;
