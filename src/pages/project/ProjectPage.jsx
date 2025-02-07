cbfd82e50781f170ad476330608b308c25b631de
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-02-07 21:48:20 +0900

    feat: 프로젝트 페이지 api 연결 코드 추가(추후 구현 예정)

--- src/pages/project/ProjectPage.jsx
+++ src/pages/project/ProjectPage.jsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
-import projects from "../../data/project/ProjectsData";
+import Projects from "../../data/project/ProjectsData";
+import ProjectData from "../../data/project/ProjectData";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Modal from "../../components/application/Modal";
 
 const ProjectContainer = styled.div`
   background: black;
@@ -53,6 +57,7 @@ const CardContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+  cursor: pointer;
 `;
 
 const Img = styled.img`
@@ -97,6 +102,41 @@ const Description = styled.div`
 `;
 
 const ProjectPage = () => {
+  const [projects, setProjects] = useState([]);
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // api 연결
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const getProjects = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/projects`);
+      setProjects(response.data);
+      console.log("프로젝트 get 요청 응답: ", response.data);
+    } catch (e) {
+      console.log("프로젝트 get 요청 실패: ", e);
+    }
+  };
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  const openModal = async (projectId) => {
+    try {
+      const response = await axios.get(`${apiUrl}/projects/${projectId}`);
+      setSelectedProject(response.data);
+      setIsOpen(true);
+    } catch (e) {
+      console.log("프로젝트 get 요청 실패: ", e);
+    }
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setSelectedProject(null);
+  };
+
   return (
     <ProjectContainer>
       <Section>
@@ -105,7 +145,7 @@ const ProjectPage = () => {
       </Section>
       <ProjectWrapper>
         {projects.map((project) => (
-          <CardContainer>
+          <CardContainer key={project.id} onClick={() => openModal(project.id)}>
             <Img src={project.img} />
             <Project>
               <ProjectName>{project.name}</ProjectName>
@@ -115,6 +155,9 @@ const ProjectPage = () => {
           </CardContainer>
         ))}
       </ProjectWrapper>
+      <Modal isOpen={isOpen} onClose={closeModal}>
+        {selectedProject && <></>}
+      </Modal>
     </ProjectContainer>
   );
 };
