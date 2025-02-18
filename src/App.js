07e9b1f7586ac490447db03543f8428c5e7bdec3
chaechaen <leece144@gmail.com>
2025-02-19 04:10:39 +0900

    fix: 갤럭시탭 감지 코드 추가 (테스트)

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import GlobalStyle from "./GlobalStyles";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routing from "./pages/Routing";
 import DeviceDetect from "./components/DeviceDetect";
-import { isMobile, isTablet } from 'react-device-detect';
+import { isMobile, isTablet } from "react-device-detect";
 
 function App() {
+  const userAgent = navigator.userAgent.toLowerCase();
+  const isTouchDevice = navigator.maxTouchPoints > 1;
+  
+  // 갤럭시탭이면서 터치가 되는 기기 (데스크탑 모드에서도 감지)
+  const isSamsungTablet =
+    userAgent.includes("samsung") &&
+    isTouchDevice &&
+    !userAgent.includes("windows");
 
-  const isMobileOrTablet = isMobile || isTablet;
+  
+  const isMobileOrTablet = isMobile || isTablet || isSamsungTablet;
 
-  return (
-    isMobileOrTablet ? (
-      <DeviceDetect />
-    ) : (
-      <Router>
-        <GlobalStyle />
-        <Routing />
-      </Router>
-    )
+  return isMobileOrTablet ? (
+    <DeviceDetect />
+  ) : (
+    <Router>
+      <GlobalStyle />
+      <Routing />
+    </Router>
   );
 }
 
